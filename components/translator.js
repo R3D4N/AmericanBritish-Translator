8e26edbac566658d7d83a55840f0affe5f5971a0
R3_D4N <bellidoangelo97@gmail.com>
2023-02-21 14:55:31 -0500

    Solved logic in translator.js and api.js

--- components/translator.js
+++ components/translator.js
@@ -4,7 +4,100 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
+    americanToBritishTranslator(text) {
+        let textArray = [text.toLowerCase()]
+        let dictionaryWord = { ...americanToBritishTitles, ...americanToBritishSpelling }
+        textArray[0] = this.translate(textArray, dictionaryWord, americanOnly, 'american')
+        if (textArray[0] === text) {
+            return {translation: 'Everything looks good to me!'}
+        }
+        return {translation: textArray[0]}
+    }
 
+    britishToAmericanTranslator(text) {
+        let textArray = [text.toLowerCase()]
+        // reverse dictionaries
+        let britishToAmericanTitles = this.reversingDict(americanToBritishTitles)
+        let britishToAmericanSpelling = this.reversingDict(americanToBritishSpelling)
+
+        let dictionaryWord = { ...britishToAmericanTitles, ...britishToAmericanSpelling }
+        textArray[0] = this.translate(textArray, dictionaryWord, britishOnly, 'british')
+        if (textArray[0] === text) {
+            return {translation: 'Everything looks good to me!'}
+        }
+        return {translation: textArray[0]}
+    }
+
+    translate(text, dictionary, onlyDictionary, time) {
+        let replaceDict = {}
+        let entrieValues = Object.entries(onlyDictionary)
+        for (const [key, value] of entrieValues) {
+            if (text[0].includes(key)) {
+                replaceDict[key] = value
+            }
+        }
+
+        let wordRegex = /[a-z]{2,4}\.|[a-z]+/ig
+        let titleRegex = /(mr|mrs|ms|mx|dr|prof)\.?/g
+        let ocurrencias = new Set(text[0].match(wordRegex))
+        for (const ocurrencia of ocurrencias) {
+            if (dictionary[ocurrencia]) {
+                let value = dictionary[ocurrencia]
+                if(titleRegex.test(ocurrencia)){
+                    value = this.capitalizeFirstLetter(value)
+                }
+                replaceDict[ocurrencia] = value
+            }
+        }
+
+        if (time == 'american') {
+            let timeRegex = /\d{1,2}:\d{1,2}/g
+            let ocurrencias = text[0].match(timeRegex)
+            if (ocurrencias) {
+                for (const ocurrencia of ocurrencias) {
+                    replaceDict[ocurrencia] = ocurrencia.replace(':', '.')
+                }
+            }
+        } else {
+            let timeRegex = /\d{1,2}\.\d{1,2}/g
+            let ocurrencias = text[0].match(timeRegex)
+            if (ocurrencias) {
+                for (const ocurrencia of ocurrencias) {
+                    replaceDict[ocurrencia] = ocurrencia.replace('.', ':')
+                }
+            }
+        }
+        if (Object.keys(replaceDict).length === 0) {
+            return this.capitalizeFirstLetter(text[0])
+        }
+        return this.replaceWord(text[0], replaceDict)
+    }
+
+    capitalizeFirstLetter(text){
+        let letterRegex = /^[a-z]/
+        if(letterRegex.test(text)){
+            return text.charAt(0).toUpperCase() + text.slice(1)
+        }
+        return text
+    }
+
+    reversingDict(dictionary) {
+        let reversedDict = {}
+        for (const [key, value] of Object.entries(dictionary)) {
+            reversedDict[value] = key
+        }
+        return reversedDict
+    }
+
+    replaceWord(text, replaceDict) {
+        let entrieValues = Object.entries(replaceDict)
+        entrieValues.forEach(([key, value]) => {
+            let span = `<span class="highlight">${value}</span>`
+            text = text.replaceAll(key, span)
+        })
+        text = this.capitalizeFirstLetter(text)
+        return text
+    }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,17 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      let {text, locale} = req.body
+      if(!locale) return res.json({error: 'Required field(s) missing'})
+      if(text.length == 0) return res.json({error: 'No text to translate'})
+      let localeRegex = /american-to-british|british-to-american/
+      if(!localeRegex.test(locale)) return res.json({error: 'Invalid value for locale field'})
+      let message = {}
+      if(locale == 'american-to-british'){
+        message = translator.americanToBritishTranslator(text)
+      }else{
+        message = translator.britishToAmericanTranslator(text)
+      }
+      res.json(message)
     });
 };
